64083b2b4e20a3558f97046545b785dee7cb4c4a
Baku Arimoto <Baku.Arimoto@gmail.com>
2023-06-04 01:38:34 +0900

    20230603

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def todo(Request):
+    returnobject = HttpResponse('hello todi')
+    return returnobject
